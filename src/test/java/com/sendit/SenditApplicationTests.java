ede23ff634bbd2789c83509e34667491ab82e3a5
munsusong <private1023@naver.com>
2020-12-08 22:59:38 +0900

    initialize

---
+++ src/test/java/com/sendit/SenditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
