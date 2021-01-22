cac9be40db03b38178be4010c9cf86f603c8732a
munsusong <private1023@naver.com>
2021-01-22 18:17:21 +0900

    업로드 테스트

--- src/main/java/com/sendit/sendit/controller/SenditController.java
+++ src/main/java/com/sendit/sendit/controller/SenditController.java
@@ -3,11 +3,16 @@ package com.sendit.sendit.controller;
 import com.sendit.sendit.dto.FileDTO;
 import com.sendit.sendit.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.List;
 
 @RestController
 @RequestMapping("/test")
@@ -19,26 +24,47 @@ public class SenditController {
         this.fileservice = fileservice;
     }
 
-    @PostMapping("/upload")
-    public String upload(@RequestParam("file") MultipartFile file) {
-        System.out.println("파일 이름 : " + file.getOriginalFilename());
-        System.out.println("파일 크기 : " + file.getSize());
+    @PostMapping(value = "/upload",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> upload(MultipartHttpServletRequest mtfRequest) {
 
-        try (
-                FileOutputStream fos = new FileOutputStream("c:/filedown/" + file.getOriginalFilename());
-                InputStream is = file.getInputStream();
-        ) {
-            int readCount = 0;
-            byte[] buffer = new byte[1024];
-            while ((readCount = is.read(buffer)) != -1) {
-                fos.write(buffer, 0, readCount);
+        //폴더 생성 여부 확인
+        String path = "D:\\filedown";
+        File Folder = new File(path);
+
+        if(!Folder.exists()){
+            try{
+                Folder.mkdir();
+                System.out.println("폴더를 생성했습니다.");
             }
-        } catch (Exception ex) {
-            throw new RuntimeException("file Save Error");
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("폴더가 생성되어있습니다.");
         }
 
 
-        return "uploadok";
+        //서버로 넘어온 파일 저장
+        List<MultipartFile> fileList = mtfRequest.getFiles("file");
+
+        for(MultipartFile mf : fileList){
+            System.out.println("파일 이름 : " + mf.getOriginalFilename());
+            System.out.println("파일 크기 : " + mf.getSize());
+
+            try (
+                    FileOutputStream fos = new FileOutputStream("D:/filedown/" + mf.getOriginalFilename());
+                    InputStream is = mf.getInputStream();
+            ) {
+                int readCount = 0;
+                byte[] buffer = new byte[1024];
+                while ((readCount = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, readCount);
+                }
+            } catch (Exception ex) {
+                throw new RuntimeException("서버 오류");
+            }
+        }
+        return ResponseEntity.ok(null);
     }
 
     @PostMapping("/test")
