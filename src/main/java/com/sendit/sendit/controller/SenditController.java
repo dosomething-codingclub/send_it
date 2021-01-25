9f71024dd65685f7f02eb0444fa30d881c451379
munsusong <private1023@naver.com>
2021-01-25 17:20:24 +0900

    파일 업로드 및 다운로드 구현(동작 확인 X)

--- src/main/java/com/sendit/sendit/controller/SenditController.java
+++ src/main/java/com/sendit/sendit/controller/SenditController.java
@@ -1,10 +1,13 @@
 package com.sendit.sendit.controller;
 
 import com.sendit.sendit.dto.FileDTO;
+import com.sendit.sendit.model.File;
+import com.sendit.sendit.repository.FileRepository;
 import com.sendit.sendit.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -12,22 +15,27 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.net.URLEncoder;
 import java.util.List;
 
 @RestController
 @RequestMapping("/test")
 public class SenditController {
     private final FileService fileservice;
+    private final FileRepository filerepository;
 
     @Autowired
-    public SenditController(FileService fileservice) {
+    public SenditController(FileService fileservice, FileRepository filerepository) {
         this.fileservice = fileservice;
+        this.filerepository = filerepository;
     }
 
-    @PostMapping(value = "/upload",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    //업로드
+    @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<?> upload(MultipartHttpServletRequest mtfRequest) {
 
         HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();//HttpServletReqeust 가져오기
@@ -35,59 +43,51 @@ public class SenditController {
         if (ip == null)//was가 아니라면
             ip = req.getRemoteAddr();
 
-        FileDTO filedto = new FileDTO();
-
-        //폴더 생성 여부 확인
-        String path = "D:\\filedown";
-        File Folder = new File(path);
-
-        if(!Folder.exists()){
-            try{
-                Folder.mkdir();
-                System.out.println("폴더를 생성했습니다.");
-            }
-            catch(Exception e){
-                e.printStackTrace();
-            }
-        }else{
-            System.out.println("폴더가 생성되어있습니다.");
-        }
-
-
-        //서버로 넘어온 파일 저장
+        //서버로 넘어온 파일들을 리스트로 저장
         List<MultipartFile> fileList = mtfRequest.getFiles("file");
 
-        for(MultipartFile mf : fileList){
-            System.out.println("파일 이름 : " + mf.getOriginalFilename());
-            System.out.println("파일 크기 : " + mf.getSize());
+        fileservice.filesave(fileservice, fileList, ip);
 
-            System.out.println(ip);
-            //아이피 설정
-            filedto.setSendIp(ip);
-            //토큰 설정
-            filedto.setToken("123456");
-            //파일명 설정
-            filedto.setFileName(mf.getOriginalFilename());
+        return ResponseEntity.ok(null);
+    }
 
-            try (
-                    FileOutputStream fos = new FileOutputStream("D:/filedown/" + mf.getOriginalFilename());
-                    InputStream is = mf.getInputStream();
-            ) {
-                int readCount = 0;
-                byte[] buffer = new byte[1024];
-                while ((readCount = is.read(buffer)) != -1) {
-                    fos.write(buffer, 0, readCount);
-                }
-            } catch (Exception ex) {
-                throw new RuntimeException("서버 오류");
-            }
-            fileservice.create(filedto);
+    //다운로드
+    @PostMapping("/download")
+    public ResponseEntity<?> download(@RequestParam("token") String token, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        List<File> temp = filerepository.findByToken(token);
+        File mFile = temp.get(0);
+        String orgName = mFile.getFileName();
+        java.io.File file = new java.io.File("D:\\filedown", orgName);
+
+        //User-Agent : 어떤 운영체제로  어떤 브라우저를 서버( 홈페이지 )에 접근하는지 확인함
+        String header = request.getHeader("User-Agent");
+        String fileName;
+
+        if ((header.contains("MSIE")) || (header.contains("Trident")) || (header.contains("Edge"))) {
+            //인터넷 익스플로러 10이하 버전, 11버전, 엣지에서 인코딩
+            fileName = URLEncoder.encode(orgName, "UTF-8");
+        } else {
+            //나머지 브라우저에서 인코딩
+            fileName = new String(orgName.getBytes("UTF-8"), "iso-8859-1");
+        }
+        //형식을 모르는 파일첨부용 contentType
+        response.setContentType("application/octet-stream");
+        //다운로드와 다운로드될 파일이름
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+
+        try (BufferedInputStream b_is = new BufferedInputStream(new FileInputStream(file));
+             BufferedOutputStream b_os = new BufferedOutputStream(response.getOutputStream())
+        ) {
+            FileCopyUtils.copy(b_is, b_os);//파일 복사
+            b_os.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return ResponseEntity.ok(null);
     }
 
     @PostMapping("/test")
-    public FileDTO test(@RequestBody FileDTO filedto){
+    public FileDTO test(@RequestBody FileDTO filedto) {
         fileservice.create(filedto);
         return filedto;
     }
--- src/main/java/com/sendit/sendit/service/FileService.java
+++ src/main/java/com/sendit/sendit/service/FileService.java
@@ -5,24 +5,104 @@ import com.sendit.sendit.model.File;
 import com.sendit.sendit.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class FileService {
+
     private final FileRepository filerepository;
 
     @Autowired
-    public FileService(FileRepository filerepository){
+    public FileService(FileRepository filerepository) {
         this.filerepository = filerepository;
     }
 
-    public List<File> getAllFile(){
+    public List<File> getAllFile() {
         return filerepository.findAll();
     }
 
-    public void create(FileDTO filedto){
+    public void create(FileDTO filedto) {
         File file = filedto.toEntity();
         filerepository.save(file);
+    }
+
+    public String maketoken() {
+        String token = "";
+        List<File> temp;
+
+        //중복된 토큰 확인 및 생성
+        while (true) {
+            if (token == "") {
+                for (int i = 0; i < 6; i++) {
+                    token += Integer.toString(new Random().nextInt(9));
+                }
+                temp = filerepository.findByToken(token);
+
+                if (temp.isEmpty()) {
+                    break;
+                } else {
+                    token = "";
+                }
+            }
+        }
+        return token;
+    }
+
+    public void mkdir() {
+        //폴더 생성 여부 확인
+        String path = "D:\\filedown";
+        java.io.File Folder = new java.io.File(path);
+
+        if (!Folder.exists()) {
+            try {
+                Folder.mkdir();
+                System.out.println("폴더를 생성했습니다.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void filesave(FileService fileservice, List<MultipartFile> fileList, String ip) {
+        //저장을 위한 폴더 생성
+        fileservice.mkdir();
+
+        FileDTO filedto = new FileDTO();
+
+        for (MultipartFile mf : fileList) {
+            System.out.println("파일 이름 : " + mf.getOriginalFilename());
+            System.out.println("파일 크기 : " + mf.getSize());
+
+            System.out.println(ip);
+            //아이피 설정
+            filedto.setSendIp(ip);
+            //토큰 설정
+            filedto.setToken(fileservice.maketoken());
+            //파일명 설정
+            filedto.setFileName(mf.getOriginalFilename());
+
+            try (
+                    FileOutputStream fos = new FileOutputStream("D:/filedown/" + mf.getOriginalFilename());
+                    InputStream is = mf.getInputStream();
+                    BufferedOutputStream b_fos = new BufferedOutputStream(fos, 1024);
+                    BufferedInputStream b_is = new BufferedInputStream(is, 1024)
+            ) {
+                int readCount = 0;
+                byte[] buffer = new byte[1024];
+                while ((readCount = b_is.read(buffer)) != -1) {
+                    b_fos.write(buffer, 0, readCount);
+                }
+            } catch (Exception ex) {
+                throw new RuntimeException("서버 오류");
+            }
+            fileservice.create(filedto);
+        }
     }
 }
